beedfa8a284ab16b2107dcf6072bd306f74a9ca1
Ulrich Lejoyeux <ulrich.lejoyeux@steeple.fr>
2018-12-10 21:46:09 +0100

    add PointNClickable class

---
+++ Scripts/PointNClickable.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointNClickable : MonoBehaviour {
+    public static PointNClickable clicked;
+
+    void OnMouseDown()
+    {
+        Interact();
+    }
+
+    public virtual void Interact()
+    {
+    
+    }
+
+    public virtual void PreClick()
+    {
+        
+    }
+}
