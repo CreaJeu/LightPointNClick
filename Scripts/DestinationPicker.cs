dca8e851e0db56c81eca63340654eb608bb76fd7
Ulrich Lejoyeux <ulrich.lejoyeux@steeple.fr>
2018-12-10 20:38:49 +0100

    first commit

---
+++ Scripts/DestinationPicker.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestinationPicker : MonoBehaviour {
+    private Collider myCollider;
+    private Camera mainCamera;
+    public static Vector3 destination;
+    public static bool newDestination;
+
+	// Use this for initialization
+	void Start () {
+        newDestination = false;
+        mainCamera = Camera.allCameras[0];
+        myCollider = GetComponent<Collider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        newDestination = false;
+        if (Input.GetMouseButtonDown(0)) {
+            //Debug.Log("zsaaszazc" + mainCamera);
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+            //Debug.Log("hejejehekzelk");
+
+            RaycastHit hit;
+            if (myCollider.Raycast(ray, out hit, 100.0f)) {
+                destination = hit.point;
+                //Debug.Log(destination);
+                newDestination = true;
+            }
+        }
+	}
+}
---
+++ Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Player : MonoBehaviour {
+    enum State
+    {
+        READY, WALKING
+    };
+
+    State state;
+
+	// Use this for initialization
+	void Start () {
+        state = State.READY;
+        //GetComponent<NavMeshAgent>().destination = new Vector3(10,0,10);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (DestinationPicker.newDestination) {
+            Debug.Log("dest ? " + DestinationPicker.newDestination + " " + DestinationPicker.destination);
+            GetComponent<NavMeshAgent>().destination = DestinationPicker.destination;
+        }
+	}
+}
